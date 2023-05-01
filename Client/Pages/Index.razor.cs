39b2527cfbf3c908174f4b25ebc3e2cf712496b1
Thomas Becker <tbecker@captechventures.com>
2023-05-01 11:24:53 -0400

    Clearing out fleshed out code

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -7,33 +7,6 @@ namespace TrainingLog.Client.Pages;
 
 public partial class Index
 {
-    [Inject]
-    private ITrainingEventManager TrainingEventManager { get; set; }
 
-    private List<TrainingEventDto> _trainingEvents = new ();
-    private bool _showAddModal;
-    private ModalWrapper AddLogEventModal{get;set;}
-    protected override async Task OnInitializedAsync()
-    {
-        await LoadTrainingEvents();
-    }
-
-    private async Task LoadTrainingEvents()
-    {
-        _trainingEvents = await TrainingEventManager.GetAllTrainingEvents();
-    }
-
-    private void ShowAddModal()
-    {
-        _showAddModal = true;
-        AddLogEventModal.Open();
-    }
-
-    private async Task TrainingLogSaved()
-    {
-        AddLogEventModal.Close();
-        _showAddModal = false;
-        await LoadTrainingEvents();
-    }
 
 }
